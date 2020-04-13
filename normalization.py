9612078bc5bef23d196531e4004c26f7f48b9e41
pdlam98 <pdlam.uit@gmail.com>
2020-04-13 22:35:09 +0700

    print avg and variance

--- normalization.py
+++ normalization.py
@@ -18,13 +18,14 @@ def zScoreMethod(column, lines, columnSelected):
 	AVERAGE = sum(columnSelected)/N
 	SUM = sum(list(map(lambda line: math.pow(float(line[column]) - AVERAGE, 2), lines)))
 	STANDARD_DEVIATION = math.sqrt((1/N)*SUM)
-
+	# print('AVERAGE: ',round(AVERAGE, 2),' - VARIANCE: ',round((1/N)*SUM, 2))
 	for line in lines:
 		v = float(line[column])
 		line[column] = str(round((v - AVERAGE)/STANDARD_DEVIATION, 2))
 
 # Change the value of the attributes
 def changeValues(method, column, lines, columnSelected):
+	print('1: ',column)
 	if method == 'min-max':
 		minMaxMethod(column, lines, columnSelected)
 	elif method == 'z-score':
@@ -34,24 +35,25 @@ def changeValues(method, column, lines, columnSelected):
 def normalization(data):
 	attributes = data[0]
 	lines = data[1:]
-
 	method = input('Enter the normalize method (type: [min-max, z-score]: ')
+	index = 0
 
 	if (method in ['min-max', 'z-score']):
 		for value in lines[0]:
 			try:
 				if isinstance(float(value), float):
-					column = lines[0].index(value)
-					columnSelected = list(map(lambda line: float(line[column]), lines))
-					changeValues(method, column, lines, columnSelected)
+					print(index)
+					columnSelected = list(map(lambda line: float(line[index]), lines))
+					changeValues(method, index, lines, columnSelected)
 			except:
 				print
+			index += 1
 	else:
 		print('The normalize method must be "min-max" or "z-score".\nPlease run the program again!')
 		return
 
 def main():
-	fInput = open('./data/data.txt', 'r')
+	fInput = open('./data/google_review_ratings.txt', 'r')
 	data = [line.rstrip('\n').split(',') for line in fInput]
 	fInput.close()
 
